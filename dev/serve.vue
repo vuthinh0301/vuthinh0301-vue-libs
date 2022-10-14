d1b0bbe399be65b02da75bb20caa2442950062da
vuthinh0301 <vuthinh0301@gmail.com>
2022-10-14 09:13:51 +0700

    init project

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { Vuthinh0301VueLibsSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  Vuthinh0301VueLibsSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vuthinh0301-vue-libs-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as Vuthinh0301VueLibsSample } from './vuthinh0301-vue-libs-sample.vue';
